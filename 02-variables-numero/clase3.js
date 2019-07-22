56cff9640aefaf0d51f28cc936285cee29253d7c
MARIO FDEZ <mario.mamonuo@gmail.com>
2019-07-22 08:27:18 -0500

    ejercicios en carpetas 01 hasta la 03

---
+++ 02-variables-numero/clase3.js
@@ -0,0 +1,31 @@
+var edad = 37
+
+// edad = edad + 2 
+
+edad += 1
+
+var peso = 75
+
+// peso = peso - 2
+
+peso -= 2
+
+
+var sandwich = 1
+peso += sandwich
+
+
+var jugarFutbol = 3
+
+peso -=jugarFutbol 
+
+var precioDeVino = 200.3
+
+var total = Math.round(precioDeVino * 100 * 3) / 100
+var totalStr = total.toFixed(3)
+var total2 = parseFloat(totalStr)
+
+var pizza = 8 
+var personas = 2
+var cantidadDePorcionesPorPersona = pizza / personas
+
---
+++ 03-funciones/clase4.js
@@ -0,0 +1,12 @@
+var nombre = 'Mario' , edad = 28
+
+function imprimirEdad(n, e){
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad ('Enrique', 28)
+imprimirEdad ('Ronald', 20)
+imprimirEdad ('Miguel', 18)
+imprimirEdad ( 27, 'Pechi')
+imprimirEdad('Serrano')
