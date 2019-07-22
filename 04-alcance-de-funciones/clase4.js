6aceee0bef2c3a3e2c79ae20f4b631970e9b3599
MARIO FDEZ <mario.mamonuo@gmail.com>
2019-07-22 08:55:43 -0500

    alcance de funciones

---
+++ 04-alcance-de-funciones/clase4.js
@@ -0,0 +1,10 @@
+var nombre = 'Mario Enrique'
+
+
+function imprimirNombreEnMayusculas(nombre){
+    
+    nombre = nombre.toUpperCase()
+    console.log(nombre)
+}
+
+imprimirNombreEnMayusculas(nombre)
